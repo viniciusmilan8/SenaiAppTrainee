0517bc2e4a6399598b8a925f101563af82e28c03
vinicius_milan1 <vinicius_milan1@SN-352084>
2023-08-05 11:54:34 -0300

    Aula 2023-08-05

--- SenaiApp/Controllers/ClienteController.cs
+++ SenaiApp/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SenaiApp.Domain.Entidade;
 
 namespace SenaiApp.Controllers
 {
@@ -10,7 +11,8 @@ namespace SenaiApp.Controllers
         }
         public IActionResult Form()
         {
-            return View();
+            var model = new Cliente();
+            return View(model);
         }
     }
 }
